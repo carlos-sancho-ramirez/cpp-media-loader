aeb38d3c7577e4b00c41bd012c269bab1a969d30
Charlie <charlie@charlie-System-Product-Name.(none)>
2014-02-06 22:59:53 +0100

    Added main file and some basic project configurations.

---
+++ src/conf.h
@@ -0,0 +1,7 @@
+
+#ifndef CONF_H_
+#define CONF_H_
+
+#define PROJECT_VERSION_STR "0.1"
+
+#endif /* CONF_H_ */
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+
+#include "conf.h"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	std::cout << "C++ Media Loader" << std::endl
+			<< "This software is under the MIT License" << std::endl
+			<< "Version " PROJECT_VERSION_STR << std::endl << std::endl;
+
+	return 0;
+}
